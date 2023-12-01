9ce574f2041a3ca76458da8cb18ee1cdec7bf2ca
ponbac <root@ponbac.xyz>
2023-12-01 23:05:11 +0100

    day2 prep

---
+++ day2/src/main.rs
@@ -0,0 +1,21 @@
+static EXAMPLE_INPUT: &str = r#"
+"#;
+
+// cargo watch -x "run -q"
+fn main() {
+    println!("-- Advent of Code 2023 - Day 2 --");
+
+    // let input = include_str!("input.txt");
+    let input = EXAMPLE_INPUT;
+
+    println!("Part 1: {}", part1(input));
+    // println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    todo!()
+}
+
+fn part2(input: &str) -> usize {
+    todo!()
+}
