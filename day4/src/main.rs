7f85ea0c90f0e28d4f29ec7c66221c31428e3950
ponbac <root@ponbac.xyz>
2023-12-03 16:45:44 +0100

    day4 prep

---
+++ day4/src/main.rs
@@ -0,0 +1,27 @@
+use nom::{character::complete::digit1, combinator::map_res, IResult};
+
+static EXAMPLE_INPUT: &str = r#"
+
+"#;
+
+fn main() {
+    println!("-- Advent of Code 2023 - Day 2 --");
+
+    let input = EXAMPLE_INPUT;
+    // let input = include_str!("input.txt");
+
+    part1(input);
+    // part2(input);
+}
+
+fn part1(input: &str) {
+    todo!()
+}
+
+fn part2(input: &str) {
+    todo!()
+}
+
+fn parse_number(i: &str) -> IResult<&str, usize> {
+    map_res(digit1, |s: &str| s.parse::<usize>())(i)
+}
