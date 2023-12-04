c5dd82d8dbd81f94afe234ec74322dc542d15958
ponbac <root@ponbac.xyz>
2023-12-04 15:11:55 +0100

    day 4 clean

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,11 @@
-use std::collections::VecDeque;
-
-use nom::{character::complete::digit1, combinator::map_res, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, multispace0, multispace1},
+    combinator::map_res,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
 
 static EXAMPLE_INPUT: &str = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -9,68 +14,73 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
 
-fn parse_card(i: &str) -> (Vec<usize>, Vec<usize>) {
-    let i = i.split(':').last().unwrap();
+#[derive(Debug)]
+struct Card {
+    matches: u32,
+}
 
-    let winning_i = i.split('|').next().unwrap();
-    let my_i = i.split('|').last().unwrap();
+fn parse_card(i: &str) -> IResult<&str, Card> {
+    let i = i.split(": ").nth(1).unwrap().trim();
+    let (i, (winning, my)) = separated_pair(
+        separated_list1(multispace1, preceded(multispace0, parse_number)),
+        tag(" | "),
+        separated_list1(multispace1, preceded(multispace0, parse_number)),
+    )(i)?;
 
-    let winning = winning_i
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
-
-    let my = my_i
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
-
-    (winning, my)
+    Ok((
+        i,
+        Card {
+            matches: my.iter().filter(|&&num| winning.contains(&num)).count() as u32,
+        },
+    ))
 }
 
 fn main() {
     println!("-- Advent of Code 2023 - Day 4 --");
 
-    let input = EXAMPLE_INPUT;
-    // let input = include_str!("input.txt");
+    // let input = EXAMPLE_INPUT;
+    let input = include_str!("input.txt");
 
     part1(input);
-    // part2(input);
+    part2(input);
 }
 
 fn part1(input: &str) {
-    let mut cards = vec![];
-    for line in input.lines() {
-        cards.push(parse_card(line));
-    }
+    let cards = input
+        .lines()
+        .map(|line| parse_card(line).unwrap().1)
+        .collect::<Vec<_>>();
 
-    let mut total_cards = 0;
-    let mut queue = VecDeque::new();
-
-    for i in 0..cards.len() {
-        queue.push_back(i);
-    }
-
-    while let Some(card_index) = queue.pop_front() {
-        total_cards += 1;
-        let (ref winning, ref my) = cards[card_index];
-
-        let matches = my.iter().filter(|&&num| winning.contains(&num)).count();
-
-        for next_card_index in card_index + 1..card_index + 1 + matches {
-            if next_card_index < cards.len() {
-                queue.push_back(next_card_index);
+    let sum = cards
+        .iter()
+        .map(|card| {
+            if card.matches > 0 {
+                2_u32.pow(card.matches - 1)
+            } else {
+                0
             }
-        }
-    }
+        })
+        .sum::<u32>();
 
-    println!("Total cards: {}", total_cards);
+    println!("Part 1: {}", sum);
 }
 
 fn part2(input: &str) {
-    todo!()
+    let cards = input
+        .lines()
+        .map(|line| parse_card(line).unwrap().1)
+        .collect::<Vec<_>>();
+
+    let mut factors = vec![1; cards.len()];
+    for (i, card) in cards.iter().enumerate() {
+        let factor = factors[i];
+        (i..i + card.matches as usize).for_each(|j| factors[j + 1] += factor);
+    }
+
+    let sum = factors.iter().sum::<u32>();
+    println!("Part 2: {}", sum);
 }
 
-fn parse_number(i: &str) -> IResult<&str, usize> {
-    map_res(digit1, |s: &str| s.parse::<usize>())(i)
+fn parse_number(i: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s: &str| s.parse::<u32>())(i)
 }
