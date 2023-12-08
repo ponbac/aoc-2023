facb9ce2b8573403bc22f87207fbee9b6dadd5b6
ponbac <root@ponbac.xyz>
2023-12-08 07:55:34 +0100

    sad start

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,16 +1,23 @@
 use std::collections::HashMap;
 
-static EXAMPLE_INPUT: &str = r#"LLR
+use num::integer::lcm;
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)"#;
+static EXAMPLE_INPUT: &str = r#"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"#;
 
 fn main() {
     println!("\n-- Advent of Code 2023 - Day 8 --");
 
-    let input = EXAMPLE_INPUT;
-    // let input = include_str!("input.txt");
+    // let input = EXAMPLE_INPUT;
+    let input = include_str!("input.txt");
 
     part1(input);
     // part2(input);
@@ -38,32 +45,20 @@ fn part1(input: &str) {
             .trim_matches(')');
 
         map.insert(key, (left, right));
+        // println!("{}: ({}, {})", key, left, right);
     }
-    let start = input.lines().nth(2).unwrap().split(" = ").next().unwrap();
-    let goal = input.lines().last().unwrap().split(" = ").next().unwrap();
+    // let start = "AAA";
+    // let goal = "ZZZ";
 
-    println!("{:?}, len {}", steps, steps.len());
-    println!("{:?}", map);
-    println!("{:?}", start);
-    println!("{:?}", goal);
+    // println!("{:?}, len {}", steps, steps.len());
+    // println!("{:?}", map);
+    // println!("{:?}", start);
+    // println!("{:?}", goal);
 
-    let mut current = start;
-    let mut n_steps = 0;
-    // repeat steps if it ends before the last step
+    // let mut current = start;
+    // let mut n_steps = 0;
     // while current != goal {
     //     let step = steps.chars().nth(n_steps % steps.len()).unwrap();
-    //     // if one of the steps is the goal, go there
-    //     if step == 'L' && map.get(current).unwrap().0 == goal {
-    //         current = goal;
-    //         n_steps += 1;
-    //         println!("Left :{}: {}", n_steps, current);
-    //         break;
-    //     } else if step == 'R' && map.get(current).unwrap().1 == goal {
-    //         current = goal;
-    //         n_steps += 1;
-    //         println!("{}: {}", n_steps, current);
-    //         break;
-    //     }
 
     //     current = match step {
     //         'L' => map.get(current).unwrap().0,
@@ -71,23 +66,38 @@ fn part1(input: &str) {
     //         _ => panic!("Unknown step: {}", step),
     //     };
     //     n_steps += 1;
-    //     println!("{}: {}", n_steps, current);
     // }
-    for curr in steps.chars().cycle() {
-        n_steps += 1;
-        let location = map.get(current).unwrap();
-        match curr {
-            'L' => current = location.0,
-            'R' => current = location.1,
-            _ => panic!("invalid direction {curr}"),
+
+    // println!("Part 1 - Steps: {}", n_steps);
+
+    let paths_ending_with_a = map
+        .iter()
+        .filter(|(key, _)| key.ends_with('A'))
+        .map(|(key, _)| *key)
+        .collect::<Vec<_>>();
+
+    println!("{:?}", paths_ending_with_a);
+
+    let mut cycles = vec![];
+    for path in paths_ending_with_a {
+        let mut current = path;
+        let mut n_steps = 0;
+        while !current.ends_with('Z') {
+            let step = steps.chars().nth(n_steps % steps.len()).unwrap();
+
+            current = match step {
+                'L' => map.get(current).unwrap().0,
+                'R' => map.get(current).unwrap().1,
+                _ => panic!("Unknown step: {}", step),
+            };
+            n_steps += 1;
         }
 
-        if current == goal {
-            break;
-        }
+        cycles.push(n_steps);
     }
 
-    println!("Steps: {}", n_steps);
+    let lcm = cycles.iter().fold(1, |acc, x| lcm(acc, *x));
+    println!("Part 2 - Steps: {}", lcm);
 }
 
 fn part2(input: &str) {
