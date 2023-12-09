d032932fa843d344759709909ca92cf1b4cfb61b
ponbac <root@ponbac.xyz>
2023-12-09 14:33:31 +0100

    d9 prep

---
+++ day9/src/main.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+use num::integer::lcm;
+
+static EXAMPLE_INPUT: &str = r#"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"#;
+
+fn main() {
+    println!("\n-- Advent of Code 2023 - Day 9 --");
+
+    let input = EXAMPLE_INPUT;
+    // let input = include_str!("input.txt");
+
+    part1(input);
+    // part2(input);
+}
+
+fn part1(input: &str) {
+    todo!()
+}
+
+fn part2(input: &str) {
+    todo!()
+}
