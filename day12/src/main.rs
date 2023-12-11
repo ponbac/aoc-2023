9e9dd612791e40a36e48132479735f7cfbebdb61
ponbac <root@ponbac.xyz>
2023-12-11 19:22:42 +0100

    d12 prep

---
+++ day12/src/main.rs
@@ -0,0 +1,20 @@
+static EXAMPLE_INPUT: &str = r#"
+"#;
+
+fn main() {
+    println!("\n-- Advent of Code 2023 - Day 12 --");
+
+    let input = EXAMPLE_INPUT;
+    // let input = include_str!("input.txt");
+
+    part1(input);
+    // part2(input);
+}
+
+fn part1(input: &str) {
+    todo!()
+}
+
+fn _part2(_input: &str) {
+    todo!()
+}
