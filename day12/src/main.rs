9b211ffa45d491ec5c420809580839ab38a4bf99
ponbac <root@ponbac.xyz>
2023-12-12 20:03:21 +0100

    better

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -12,51 +12,44 @@ static EXAMPLE_INPUT: &str = r#"???.### 1,1,3
 fn main() {
     println!("\n-- Advent of Code 2023 - Day 12 --");
 
-    let input = EXAMPLE_INPUT;
-    // let input = include_str!("input.txt");
+    // let input = EXAMPLE_INPUT;
+    let input = include_str!("input.txt");
 
-    // rayon::ThreadPoolBuilder::new()
-    //     .num_threads(1)
-    //     .build_global()
-    //     .unwrap();
-
-    // let start = std::time::Instant::now();
-    // part1(input);
-    // println!("Time: {:?}\n", start.elapsed());
+    let start = std::time::Instant::now();
+    part1(input);
+    println!("Time: {:?}\n", start.elapsed());
+    let start = std::time::Instant::now();
     part2(input);
+    println!("Time: {:?}\n", start.elapsed());
 }
 
-// fn part1(input: &str) {
-//     let lines: Vec<(Vec<u8>, Vec<u8>)> = input
-//         .lines()
-//         .map(|line| {
-//             let mut parts = line.split_whitespace();
-//             let pattern = parts.next().unwrap().chars().map(|c| c as u8).collect();
-//             let numbers = parts
-//                 .next()
-//                 .unwrap()
-//                 .split(',')
-//                 .map(|n| n.parse::<u8>().unwrap())
-//                 .collect();
-//             (pattern, numbers)
-//         })
-//         .collect();
+fn part1(input: &str) {
+    let lines: Vec<(Vec<u8>, Vec<usize>)> = input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let pattern = parts.next().unwrap().chars().map(|c| c as u8).collect();
+            let numbers = parts
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+            (pattern, numbers)
+        })
+        .collect();
 
-//     let arrangement_sums = (0..lines.len())
-//         .into_par_iter()
-//         .enumerate()
-//         .map(|(i, _)| {
-//             let (pattern, numbers) = &lines[i];
-//             let count = count_arrangements(
-//                 pattern, numbers, 0, //, &mut HashSet::new()
-//             );
-//             println!("{}: {:?}", i, pattern);
-//             count
-//         })
-//         .collect::<Vec<_>>();
+    let arrangements_sum = (0..lines.len())
+        .into_par_iter()
+        .enumerate()
+        .map(|(i, _)| {
+            let (pattern, numbers) = &lines[i];
+            count_arrangements(pattern, numbers, 0, 0, 0, &mut HashMap::new())
+        })
+        .sum::<usize>();
 
-//     println!("Part 1: {:?}", arrangement_sums.iter().sum::<usize>());
-// }
+    println!("Part 1: {:?}", arrangements_sum);
+}
 
 fn part2(_input: &str) {
     let lines: Vec<(Vec<u8>, Vec<usize>)> = _input
@@ -88,7 +81,7 @@ fn part2(_input: &str) {
         .map(|(i, _)| {
             let (pattern, numbers) = &lines[i];
             let count = count_arrangements(pattern, numbers, 0, 0, 0, &mut HashMap::new());
-            println!("{}, finished: {}", i, count);
+            // println!("{}, finished: {}", i, count);
             count
         })
         .sum::<usize>();
@@ -100,7 +93,7 @@ fn part2(_input: &str) {
 struct MemoKey {
     pattern_index: usize,
     number_index: usize,
-    current_count: usize,
+    damaged_count: usize,
 }
 
 fn count_arrangements(
@@ -108,91 +101,76 @@ fn count_arrangements(
     numbers: &[usize],
     pattern_index: usize,
     number_index: usize,
-    current_count: usize,
+    damaged_count: usize,
     cache: &mut HashMap<MemoKey, usize>,
 ) -> usize {
-    if let Some(&count) = cache.get(&MemoKey {
+    if let Some(&arrangements) = cache.get(&MemoKey {
         pattern_index,
         number_index,
-        current_count,
+        damaged_count,
     }) {
-        return count;
+        return arrangements;
     }
 
     let mut pattern_index = pattern_index;
     let mut number_index = number_index;
-    let mut current_count = current_count;
+    let mut damaged_count = damaged_count;
 
     loop {
-        if pattern_index >= pattern.len() {
-            if (number_index > numbers.len() - 1 && current_count == 0)
-                || (number_index == numbers.len() - 1 && current_count == numbers[number_index])
-            {
-                cache.insert(
-                    MemoKey {
-                        pattern_index,
-                        number_index,
-                        current_count,
-                    },
-                    1,
-                );
-                return 1;
-            }
+        // if end of pattern reached
+        if pattern_index == pattern.len() {
+            let valid_end = (number_index == numbers.len() && damaged_count == 0) // gone past but no damage
+                || (number_index == numbers.len() - 1 && damaged_count == numbers[number_index]); // at last number and correct damage
 
+            let result = if valid_end { 1 } else { 0 };
             cache.insert(
                 MemoKey {
                     pattern_index,
                     number_index,
-                    current_count,
+                    damaged_count,
                 },
-                0,
+                result,
             );
-            return 0;
+            return result;
         }
 
         match pattern[pattern_index] {
             b'?' => break,
             b'#' => {
-                if number_index > numbers.len() - 1 {
+                let invalid_damage =
+                    number_index > numbers.len() - 1 || damaged_count > numbers[number_index];
+
+                if invalid_damage {
                     cache.insert(
                         MemoKey {
                             pattern_index,
                             number_index,
-                            current_count,
+                            damaged_count,
                         },
                         0,
                     );
                     return 0;
                 }
 
-                if current_count == numbers[number_index] {
-                    cache.insert(
-                        MemoKey {
-                            pattern_index,
-                            number_index,
-                            current_count,
-                        },
-                        0,
-                    );
-                    return 0;
-                }
-
-                current_count += 1;
+                damaged_count += 1;
                 pattern_index += 1;
             }
             b'.' => {
-                if current_count == 0 {
+                if damaged_count == 0 {
+                    // group not started yet, skip
                     pattern_index += 1;
-                } else if current_count == numbers[number_index] {
+                } else if damaged_count == numbers[number_index] {
+                    // group finished, look for the next one
                     pattern_index += 1;
-                    current_count = 0;
                     number_index += 1;
+                    damaged_count = 0;
                 } else {
+                    // invalid end of group
                     cache.insert(
                         MemoKey {
                             pattern_index,
                             number_index,
-                            current_count,
+                            damaged_count,
                         },
                         0,
                     );
@@ -203,11 +181,12 @@ fn count_arrangements(
         }
     }
 
-    let mut total = 0;
+    // reached a '?' in the pattern
+    let mut arrangements = 0;
 
-    if current_count == 0 {
+    if damaged_count == 0 {
         if number_index < numbers.len() {
-            total +=
+            arrangements +=
                 count_arrangements(pattern, numbers, pattern_index + 1, number_index, 1, cache)
                     + count_arrangements(
                         pattern,
@@ -218,13 +197,13 @@ fn count_arrangements(
                         cache,
                     );
         } else {
-            total +=
+            // no more numbers, only valid if no damage
+            arrangements +=
                 count_arrangements(pattern, numbers, pattern_index + 1, number_index, 0, cache);
         }
-    } else if number_index > numbers.len() - 1 {
-        total += 0;
-    } else if current_count == numbers[number_index] {
-        total += count_arrangements(
+    } else if damaged_count == numbers[number_index] {
+        // valid amount of damage, go to next number
+        arrangements += count_arrangements(
             pattern,
             numbers,
             pattern_index + 1,
@@ -233,12 +212,13 @@ fn count_arrangements(
             cache,
         );
     } else {
-        total += count_arrangements(
+        // '?' needs to be damaged
+        arrangements += count_arrangements(
             pattern,
             numbers,
             pattern_index + 1,
             number_index,
-            current_count + 1,
+            damaged_count + 1,
             cache,
         );
     }
@@ -247,9 +227,9 @@ fn count_arrangements(
         MemoKey {
             pattern_index,
             number_index,
-            current_count,
+            damaged_count,
         },
-        total,
+        arrangements,
     );
-    total
+    arrangements
 }
