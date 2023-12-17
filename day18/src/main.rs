ecb8760607e79294c4a4a5d6f2404af3e00c6fef
ponbac <root@ponbac.xyz>
2023-12-17 21:22:45 +0100

    d18 prep

---
+++ day18/src/main.rs
@@ -0,0 +1,16 @@
+static EXAMPLE_INPUT: &str = r#"
+
+"#;
+
+fn main() {
+    println!("\n-- Advent of Code 2023 - Day 18 --");
+
+    let input = EXAMPLE_INPUT;
+    // let input = include_str!("input.txt");
+
+    solve(input.trim());
+}
+
+fn solve(input: &str) {
+    todo!()
+}
